fb79fdb7f847f7b137804e4ac705670b49b8faf7
Samir <follinday@gmail.com>
2026-03-28 14:43:20 +0600

    homework 9

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 
-function User(firstName, lastName) {
-  console.log(`Здравствуйте, ${lastName} ${firstName}. Рады вас видеть на курсах MetaLabs.`);
+// function User(firstName, lastName) {
+//   console.log(`Здравствуйте, ${lastName} ${firstName}. Рады вас видеть на курсах MetaLabs.`);
+// }
+
+// User("Самир", "Артыкбаев");
+
+
+const yoda = (num) => {
+
+    if (num % 3 === 0) {
+        return 'Master'
+    }
+    else if (num % 5 === 0) {
+        return 'Yoda'
+    }
+    else if (num % 3 === 0 && a % 5 === 0) {
+        return 'MasterYoda'
+    }
+    else {
+        return num;
+    }
+
 }
 
-User("Самир", "Артыкбаев");
+console.log(yoda(9));
+console.log(yoda(10));
+console.log(yoda(15));
+console.log(yoda(7));
