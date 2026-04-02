59cd27fe6bd882b585c1e9123ac713ab18d25f3f
Samir <follinday@gmail.com>
2026-04-02 10:19:15 +0600

    js home work 02.04.26

--- main.js
+++ main.js
@@ -6,24 +6,50 @@
 // User("Самир", "Артыкбаев");
 
 
-const yoda = (num) => {
+// const yoda = (num) => {
 
-    if (num % 3 === 0) {
-        return 'Master'
-    }
-    else if (num % 5 === 0) {
-        return 'Yoda'
-    }
-    else if (num % 3 === 0 && a % 5 === 0) {
-        return 'MasterYoda'
-    }
-    else {
-        return num;
-    }
+//     if (num % 3 === 0) {
+//         return 'Master'
+//     }
+//     else if (num % 5 === 0) {
+//         return 'Yoda'
+//     }
+//     else if (num % 3 === 0 && a % 5 === 0) {
+//         return 'MasterYoda'
+//     }
+//     else {
+//         return num;
+//     }
 
-}
+// }
 
-console.log(yoda(9));
-console.log(yoda(10));
-console.log(yoda(15));
-console.log(yoda(7));
+// console.log(yoda(9));
+// console.log(yoda(10));
+// console.log(yoda(15));
+// console.log(yoda(7));
+
+
+
+
+
+// 1.  У вас есть массив чисел. Создайте новый массив, где каждое число возведено в квадрат.
+// [2, 4, 6, 8];
+// // Ожидаемый результат: [4, 16, 36, 64]
+// 2. Из массива возрастов оставьте только те, чей возраст больше 27
+// [12, 18, 27, 39, 40]
+// // Ожидаемый результат [27, 39, 40]
+
+
+// 1
+// const arr = [2, 4, 6, 8]
+
+// const arrMap = arr.map(arr => arr ** 2);
+
+// console.log (arrMap)
+
+// 2
+// const ageUser = [12, 18, 27, 39, 40];
+
+// const ageOld = ageUser.filter(ageUser=> ageUser >= 27)
+
+// console.log(ageOld)
